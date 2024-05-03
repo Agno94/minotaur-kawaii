e1f6b4174a56d85d7abd099c2182600bcf5f3d73
Andrea Agnolin <andreaagnolin@phinergy.biz>
2024-05-04 01:31:25 +0200

    add logs during boot unlock

--- root-tpm-unlock/tpm2-cryptsetup.sh
+++ root-tpm-unlock/tpm2-cryptsetup.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-[ "$CRYPTTAB_TRIED" -lt "1" ] && exec tpm2-initramfs-tool unseal --pcrs 0,1,2,3,5,7
+if [ "$CRYPTTAB_TRIED" -lt "1" ]
+then
+  echo "Cryptsetup script" 1>&2
+  echo "Trying unlock $CRYPTTAB_SOURCE ($CRYPTTAB_NAME) with TPM" 1>&2
+  echo "Trying unlock $CRYPTTAB_SOURCE ($CRYPTTAB_NAME) with TPM" > /dev/kmsg
+  tpm2-initramfs-tool unseal --pcrs 0,1,2,3,5,7 --banks SHA256
+  echo Returning secret 1>&2
+  echo Returning secret > /dev/kmsg
+  exit 0
+fi
 
 /usr/bin/askpass "Passphrase for $CRYPTTAB_SOURCE ($CRYPTTAB_NAME): "
 
